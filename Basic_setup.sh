20dbf708c12161ce55017dd22b6f8a02e2a9b9b1
André Massa <andrefmassa@hotmail.con>
2021-01-20 19:34:30 -0100

    bluh

--- Basic_setup.sh
+++ Basic_setup.sh
@@ -13,7 +13,22 @@ sleep 2
 echo "- Let's get serious!"
 echo ""
 sleep 1
-echo ""
 echo "Updating everything..."
-echo "wait"
+echo ""
+sleep 1
 apt -y update && apt -y upgrade
+
+clear
+echo ""
+echo "Installing packages..."
+echo ""
+sleep 2
+apt -y vim git locate tree htop net-tools unzip tasksel fish neofetch
+apt -y update && apt -y upgrade
+echo ""
+echo "All done!!!"
+echo ""
+echo "Goodbye :)"
+sleep 3
+clear
+exit
